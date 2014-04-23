62b177b51f93bc8de7a41e822d6b8eea9ddbced6
Hong Shick Pak <hongshick.pak@gmail.com>
2014-04-23 18:32:04 -0400

    added flanger file

---
+++ source/flanger.sv
@@ -0,0 +1,12 @@
+// $Id: $
+// File name:   flanger.sv
+// Created:     4/21/2014
+// Author:      Hong Shick Pak
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: .
+module flanger(
+  input wire [31:0] data  
+);
+
+endmodule
