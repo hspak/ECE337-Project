c000368d4354c14097bd53d199469bec3139bfff
Mentor Graphics <pgohier@purdue.edu>
2014-04-24 12:26:06 -0400

    uncommented & modified fader code to insert rollover val & flag in up/down counter

--- source/fader.sv
+++ source/fader.sv
@@ -4,14 +4,14 @@
 // Author:      Patrick Gohier
 // Lab Section: 337-04
 // Version:     1.0  Initial Design Entry
-// Description: Fader Top level (& state machine)
+// Description: Fader Top level
 module fader (
     input wire clk,
     input wire n_reset,
-    input wire clear;
-    input wire fad_enable;
-    input wire [31:0] signal_in;
-    output wire [31:0] signal_out;
+    input wire clear,
+    input wire fad_enable,
+    input wire [31:0] signal_in,
+    output wire [31:0] signal_out
 );
   
   reg [21:0] mult_val;//mult_val is value (out of 1,411,233 or 21'b101011000100010100001) that the signal is being multiplied by
@@ -19,9 +19,9 @@ module fader (
   fader_state POOP(
     .clk(clk),
     .n_reset(n_reset),
-    .clear(clear);
-    .fad_enable(fad_enable);
-    .countVal(mult_val);  
+    .clear(clear),
+    .fad_enable(fad_enable),
+    .countVal(mult_val)
   );
   
   fader_wrapper POO(
