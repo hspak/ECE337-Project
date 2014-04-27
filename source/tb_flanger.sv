e925fcdce9b5445f926f3f0f7e05c66408a4170b
Hong Shick Pak <hongshick.pak@gmail.com>
2014-04-27 15:48:03 -0400

    new flanger logic

---
+++ source/tb_flanger.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   tb_flanger.sv
+// Created:     4/27/2014
+// Author:      Hong Shick Pak
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: .
