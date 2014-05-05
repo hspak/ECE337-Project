c1d9844a222d86f4a10377e9a8a409dc053d12cf
Hong Shick Pak <hongshick.pak@gmail.com>
2014-05-04 23:08:57 -0400

    mapped try number 3

---
+++ source/daf_design.sv
@@ -0,0 +1,188 @@
+// $Id: $
+// File name:   daf.sv
+// Created:     4/29/2014
+// Author:      Hong Shick Pak
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: top level
+module daf_design(
+	   input reg clk,
+	   input reg n_rst,
+     input reg ws_in,
+	   input reg [3:0] pot_vol,
+	   input reg [3:0] pot_amp_com,
+	   input reg [3:0] pot_amp_clp,
+	   input reg [3:0] swch_mode_en,  // order: flanger : amp clip : amp comp : fader
+     input reg mem_clr,
+     input reg serial_data_in,
+     output reg serial_data_out,
+     output reg ws_out,
+     output reg sck
+);
+
+  // intermmediate
+  reg [31:0] parallel_i2s;
+
+  reg sram_rw;
+  reg [31:0] sram_read_data;
+
+  reg clk_div;
+  reg shift_en;
+
+  reg [31:0] parallel_buf_vol;
+  reg [31:0] parallel_vol;
+
+  reg [31:0] parallel_buf_flang;
+  reg [31:0] parallel_flang;
+
+  reg [31:0] parallel_buf_ampclp;
+  reg [31:0] parallel_ampclp;
+  reg [31:0] parallel_ampclp_out;
+
+  reg [31:0] parallel_buf_ampcom;
+  reg [31:0] parallel_ampcom;
+  reg [31:0] parallel_ampcom_out;
+
+  reg [31:0] parallel_buf_fad;
+  reg [31:0] parallel_fad;
+
+  reg [31:0] parallel_buf_trns;
+
+  // enables for amp clp/amp com
+  assign parallel_ampclp = (swch_mode_en[2] ? parallel_ampclp_out : parallel_buf_ampclp);
+  assign parallel_ampcom = (swch_mode_en[1] ? parallel_ampcom_out : parallel_buf_ampcom);
+   
+  clk_div DIVIDER(
+    .clk(clk),
+    .n_rst(n_rst),
+    .clk_div(clk_div),
+    .shift_en(shift_en)
+  );
+
+  i2s_rcvr I2SRCVR(
+    .clk(clk),
+    .n_rst(n_rst),
+    .ws(ws_in),
+    .serial_data(serial_data_in),
+    .parallel_data(parallel_i2s)
+  );
+
+  // ENTRY POINT
+  flex_buffer #(32) BVOL(
+    .clk(clk_div),
+    .n_rst(n_rst),
+    .input_data(parallel_i2s),
+    .output_data(parallel_buf_vol)
+  );
+
+  volume_mult VOL1(
+    .channel_data(parallel_buf_vol[31:16]),
+    .pot_data(pot_vol),
+    .output_data(parallel_vol[31:16])
+  );
+  volume_mult VOL2(
+    .channel_data(parallel_buf_vol[15:0]),
+    .pot_data(pot_vol),
+    .output_data(parallel_vol[15:0])
+  );
+
+  flex_buffer #(32) BFLANG(
+    .clk(clk_div),
+    .n_rst(n_rst),
+    .input_data(parallel_vol),
+    .output_data(parallel_buf_flang)
+  );
+
+  flanger FLANG(
+    .clk(clk),
+    .n_rst(n_rst),
+    .flanger_en(swch_mode_en[3]),
+    .shift_en(shift_en),
+    .input_data(parallel_buf_flang),
+    .read_data(sram_read_data),
+    .output_data(parallel_flang),
+    .sram_rw(sram_rw)
+  );
+  sram_controller SRAMC(
+    .clk(clk),
+    .n_rst(n_rst),
+    .rw_trigger(sram_rw),
+    .mem_clr(mem_clr),
+    .mem_init(1'b0),
+    .mem_dump(1'b0),
+    .write_data(parallel_buf_flang), // direct input to sram also
+    .sram_data(sram_read_data)
+  );
+
+  flex_buffer #(32) BAMPCLP(
+    .clk(clk_div),
+    .n_rst(n_rst),
+    .input_data(parallel_flang),
+    .output_data(parallel_buf_ampclp)
+  );
+
+  amp_clip AMPCLP1(
+      .inchan(parallel_buf_ampclp[31:16]),
+      .comp_in(pot_amp_clp),
+      .outchan(parallel_ampclp_out[31:16])
+  );
+  amp_clip AMPCLP2(
+      .inchan(parallel_buf_ampclp[15:0]),
+      .comp_in(pot_amp_clp),
+      .outchan(parallel_ampclp_out[15:0])
+  );
+
+  flex_buffer #(32) BAMPCOMP(
+    .clk(clk_div),
+    .n_rst(n_rst),
+    .input_data(parallel_ampclp),
+    .output_data(parallel_buf_ampcom)
+  );
+
+  amp_comp AMPCMP1(
+    .clk(clk_div),
+    .n_rst(n_rst),
+    .inchan(parallel_buf_ampcom[31:16]),
+    .in_thresh(pot_amp_com),
+    .outchan(parallel_ampcom_out[31:16])
+  );
+  amp_comp AMPCMP2(
+    .clk(clk_div),
+    .n_rst(n_rst),
+    .inchan(parallel_buf_ampcom[15:0]),
+    .in_thresh(pot_amp_com),
+    .outchan(parallel_ampcom_out[15:0])
+  );
+  
+  flex_buffer #(32) BFAD(
+    .clk(clk_div),
+    .n_rst(n_rst),
+    .input_data(parallel_ampcom),
+    .output_data(parallel_buf_fad)
+  );
+
+  fader FAD(
+    .clk(clk),
+    .n_reset(n_rst),
+    .clear(1'b0),
+    .fad_enable(swch_mode_en[0]),
+    .signal_in(parallel_buf_fad),
+    .signal_out(parallel_fad)
+  );  
+
+  flex_buffer #(32) BUFTRNS(
+    .clk(clk),
+    .n_rst(n_rst),
+    .input_data(parallel_fad),
+    .output_data(parallel_buf_trns)
+  );
+
+  i2s_trnmtr I2STRNS(
+    .clk(clk),
+    .n_rst(n_rst),
+    .parallel_data(parallel_buf_trns),
+    .ws_flag(ws_out),
+    .serial_data(serial_data_out),
+    .sck(sck)
+  );
+endmodule
---
+++ source/tb_daf_design.sv
@@ -0,0 +1,115 @@
+// $Id: $
+// File name:   tb_daf.sv
+// Created:     4/29/2014
+// Author:      Hong Shick Pak
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: tb top
+
+`timescale 1ns / 100ps
+
+module tb_daf_design();
+  localparam CLK_PERIOD = 708;
+
+  // localparam INPUT_FILE = "./raw1";
+  // localparam OUTPUT_FILE = "./outraw_full";
+
+  // input
+  reg tb_clk;
+  reg tb_n_rst;
+  reg tb_serial_input;
+  reg ws_in;
+  reg mem_clr;
+
+  // external
+  // order: flanger : amp clip : amp comp : fader
+  reg [3:0] swch_mode_en;
+  reg [3:0] pot_vol;
+  reg [3:0] pot_amp_clp;
+  reg [3:0] pot_amp_com;
+
+  // intermmediate
+  // reg [31:0] test_in;
+  // reg [31:0] test_out;
+
+  // tb
+  // integer in_file;
+  // integer out_file;
+  // integer i;
+  // integer j;
+
+  // output
+  reg tb_serial_output;
+  reg tb_sck;
+  reg ws_out;
+
+  daf_design DAFF(
+    .clk(tb_clk),
+    .n_rst(tb_n_rst),
+    .pot_vol(pot_vol),
+    .pot_amp_clp(pot_amp_clp),
+    .pot_amp_com(pot_amp_com),
+    .swch_mode_en(swch_mode_en),
+    .mem_clr(mem_clr),
+    .serial_data_in(tb_serial_input),
+    .serial_data_out(tb_serial_output),
+    .ws_in(ws_in),
+    .ws_out(ws_out),
+    .sck(tb_sck)
+  );
+
+  // always begin
+    // tb_clk = 1'b1;
+    // #(CLK_PERIOD/2);
+    // tb_clk = 1'b0;
+    // #(CLK_PERIOD/2);
+  // end
+
+  // initial begin
+    // tb_n_rst = 1'b0;
+    // pot_vol = 4'b1010;
+    // pot_amp_com = 4'b1111;
+    // pot_amp_clp = 4'b1111;
+    // swch_mode_en[0] = 1'b1;//flanger
+    // swch_mode_en[1] = 1'b1;//amp_clip
+    // swch_mode_en[2] = 1'b1;//amp_comp
+    // swch_mode_en[3] = 1'b1;//fader
+
+    // mem_clr = 1'b0;
+    // mem_init = 1'b0;
+    // mem_dump = 1'b0;
+    // @(posedge tb_clk);
+    // mem_clr = 1'b1;
+    // @(posedge tb_clk);
+    // mem_clr = 1'b0;
+    // tb_n_rst = 1'b1;
+    // @(posedge tb_clk);
+
+    // // start
+    // in_file = $fopen(INPUT_FILE, "rb");
+    // out_file = $fopen(OUTPUT_FILE, "wb");
+
+    // while (!$feof(in_file)) begin
+      // // input
+      // $fscanf(in_file, "%c", test_in[7:0]);
+      // $fscanf(in_file, "%c", test_in[15:8]);
+      // $fscanf(in_file, "%c", test_in[23:16]);
+      // $fscanf(in_file, "%c", test_in[31:24]);
+
+      // // serialize
+      // for (i = 0; i < 34; i = i + 1) begin
+        // if (i != 16 && i != 33) begin
+          // tb_serial_input = test_in[j];
+          // j = j + 1;
+        // end
+        // @(posedge tb_clk);
+      // end
+      // $fwrite(out_file, "%c", test_out[7:0]);
+      // $fwrite(out_file, "%c", test_out[15:8]);
+      // $fwrite(out_file, "%c", test_out[23:16]);
+      // $fwrite(out_file, "%c", test_out[31:24]);
+    // end
+    // $fclose(out_file);                                                                                                            
+    // $fclose(in_file);
+  // end
+endmodule
